0486f68655690a556c7c3635ac1d5d10eea5d3f3
Momin Imran <webdevbymomin@gmail.com>
2024-10-03 21:55:47 -0400

    Refactored some code

--- app/src/components/Quiz.jsx
+++ app/src/components/Quiz.jsx
@@ -19,7 +19,7 @@ const Quiz = ({searchTerm}) => {
     
         const fetchData = async () => {
           try {
-            const response = await fetch('ai-quiz-fullstack-app-production.up.railway.app/api/generateQuiz', {
+            const response = await fetch('https://ai-quiz-fullstack-app-production.up.railway.app/api/generateQuiz', {
               method: 'POST',
               headers: {
                 'Content-Type': 'application/json',
